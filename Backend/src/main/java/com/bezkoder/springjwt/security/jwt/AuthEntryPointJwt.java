6baa500085fe1d88cac86ec5af3e018547cc3736
Aditi Agarwal <31546143+aditiagarwal34550@users.noreply.github.com>
2021-04-16 15:20:48 +0530

    Update AuthEntryPointJwt.java

--- Backend/src/main/java/com/bezkoder/springjwt/security/jwt/AuthEntryPointJwt.java
+++ Backend/src/main/java/com/bezkoder/springjwt/security/jwt/AuthEntryPointJwt.java
@@ -18,10 +18,33 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 	private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
 
 	@Override
-	public void commence(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException authException) throws IOException, ServletException {
-		logger.error("Unauthorized error: {}", authException.getMessage());
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
-	}
+        public void commence(HttpServletRequest request, HttpServletResponse response,
+                        AuthenticationException authException) throws IOException, ServletException {
+
+                   final String expired = (String) request.getAttribute("expired");
+        final String malformed = (String) request.getAttribute("malformed");
+        final String signature = (String) request.getAttribute("signature");
+        final String unsupported = (String) request.getAttribute("unsupported");
+        final String illegalargument = (String) request.getAttribute("illegalargument");
+        final String exception = (String) request.getAttribute("exception");
+        if(expired != null){
+
+            response.sendError(response.SC_UNAUTHORIZED,expired);
+        }else if (malformed !=null){
+
+            response.sendError(response.SC_UNAUTHORIZED,malformed);
+        }else if (signature !=null){
+            response.sendError(response.SC_UNAUTHORIZED,signature);
+        }else if (unsupported !=null){
+            response.sendError(response.SC_UNAUTHORIZED,unsupported);
+        }else if (illegalargument !=null){
+            response.sendError(response.SC_UNAUTHORIZED,illegalargument);
+        }else if(exception !=null ){
+            response.sendError(response.SC_UNAUTHORIZED,exception);
+        } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+        }
+
+        }
 
 }
